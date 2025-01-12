654945d6dcc394bedcbc531b78487abfdac684b6
xgeovanedamasceno <xgeovanedamasceno@outlook.com>
2025-01-12 11:05:50 -0300

    finished first section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     container: {
       center: true,
     },
-    extend: {},
+    extend: {
+      colors: {
+        verde: {
+          200: '#ACEF75',
+          300: '#91EE77',
+          400: '#17E880',
+          700: '#2E482C',
+          800: '#16281F',
+          900: '#0F1C15',
+          950: '#030504',
+        },
+      },
+    },
   },
   plugins: [],
 }
